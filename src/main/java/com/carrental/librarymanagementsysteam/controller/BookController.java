5667e17f6c2115a97cd58b928805f27e9d886182
Artash Shaghoyan <shagoyan99@gmail.com>
2026-02-03 23:24:30 +0400

    Update

--- src/main/java/com/carrental/librarymanagementsysteam/controller/BookController.java
+++ src/main/java/com/carrental/librarymanagementsysteam/controller/BookController.java
@@ -9,7 +9,6 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
--- src/main/java/com/carrental/librarymanagementsysteam/controller/MemberController.java
+++ src/main/java/com/carrental/librarymanagementsysteam/controller/MemberController.java
@@ -9,13 +9,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -33,24 +31,21 @@ public class MemberController {
 
     @GetMapping("/members/details")
     public String getMemberDetails(@RequestParam("id") int id, Model model) {
-
-        Optional<Member> optionalMember = memberRepository.findById(id);
-        if (optionalMember.isPresent()) {
-            Member member = optionalMember.get();
+        memberRepository.findById(id).ifPresent(member -> {
             model.addAttribute("member", member);
             model.addAttribute("borrowedBooks", member.getBorrowedBooks());
-            return "member-details";
-        }
-
-        return "redirect:/members";
+        });
+        return "member-details";
     }
 
     @PostMapping("/members/borrow")
     public String borrowBook(@RequestParam("memberId") int memberId, @RequestParam("bookId") int bookId) {
-        Member member = memberRepository.findById(memberId).orElseThrow();
-        Book book = bookRepository.findById(bookId).orElseThrow();
-        member.getBorrowedBooks().add(book);
-        memberRepository.save(member);
+        memberRepository.findById(memberId).ifPresent(member -> {
+            bookRepository.findById(bookId).ifPresent(book -> {
+                member.getBorrowedBooks().add(book);
+                memberRepository.save(member);
+            });
+        });
         return "redirect:/members";
     }
 
@@ -74,14 +69,12 @@ public class MemberController {
 
     @GetMapping("/members/details/delete")
     public String deleteBorrowedBook(@RequestParam("memberId") int memberId, @RequestParam("bookId") int bookId) {
-        Optional<Member> optionalMember = memberRepository.findById(memberId);
-        if (optionalMember.isPresent()) {
-            Member member = optionalMember.get();
+        memberRepository.findById(memberId).ifPresent(member -> {
             List<Book> borrowedBooks = member.getBorrowedBooks();
             borrowedBooks.removeIf(book -> book.getId() == bookId);
             member.setBorrowedBooks(borrowedBooks);
             memberRepository.save(member);
-        }
+        });
         return "redirect:/members/details?id=" + memberId;
     }
 }
